3c78781ac24a62c4ef9bfad3b270528c1c5bf2a4
SWEETPOT4T0 <lisk1223@gmail.com>
2025-01-09 16:43:40 +0900

    first

--- src/main/java/com/beyond/basic/b1_hello/domain/Hello.java
+++ src/main/java/com/beyond/basic/b1_hello/domain/Hello.java
@@ -3,12 +3,12 @@ package com.beyond.basic.b1_hello.domain;
 
 import lombok.*;
 
-@Getter
+//@Getter
 @NoArgsConstructor   //기본 생성자
 @AllArgsConstructor  //모든 매개변수 있는 생성자
-//@Data : getter, setter, toString까지 포함하는 어노테이션
+@Data //: getter, setter, toString까지 포함하는 어노테이션
 public class Hello {
     private String name;
-    @Setter  //email 변수에 관한 setter만 생성
+//    @Setter  //email 변수에 관한 setter만 생성
     private String email;
 }
